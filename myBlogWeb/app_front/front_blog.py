089b3f99843370a1c48f70977778eecdbd142608
Little-Frog-233 <1342468180@qq.com>
2020-04-16 16:32:35 +0800

    update

--- myBlogWeb/app_front/front_blog.py
+++ myBlogWeb/app_front/front_blog.py
@@ -1,9 +1,20 @@
 #coding:utf-8
+import os
+import configparser
 from flask import session, request, Response, redirect, url_for, flash, render_template
 from app_front import app_front_blue
 
+current_path = os.path.realpath(__file__)
+root_path = os.path.dirname(os.path.dirname(current_path))
+cfp_path = os.path.join(os.path.dirname(root_path), 'conf/web.conf')
+cfp = configparser.ConfigParser()
+cfp.read(cfp_path, encoding='utf-8')
+manager_id = cfp.get('web', 'manager_id')
+
 @app_front_blue.route('/')
 def index():
+    if 'manager_id' not in session:
+        session['manager_id'] = manager_id
     return render_template('index.html')
 
 @app_front_blue.route('/blog_detail/')
